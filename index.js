1e137bc955ad21564317e2d9e3f0202192f90abc
TRUNG Q HOANG <trunghoang@harviton.com>
2018-08-14 11:35:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ function theBeatlesPlay(musicians,instruments) {
   }
   return array;
 }
-const facts = ["He was the last Beatle to learn to drive","He was never a vegetarian","He was a choir boy and boy scout","He hated the sound of his own voice"
+const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"
 ];
 function johnLennonFacts(facts) {
   var i = 0;
+  var array=[]
   while (i < 4) {
-    return (facts[i] + "!!!")
+    array[i]=(facts[i] + "!!!")
     i++;
   }
   return facts;
