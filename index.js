4e904fe74dc199b0fd171d7368c3bf0053606470
TRUNG Q HOANG <trunghoang@harviton.com>
2018-08-14 10:55:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+var i;
+function theBeatlesPlay(musicians,instruments) {
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  for (i=0; i < 4; i++) {
+      return `{musicians[i]} + " plays " + {instruments[i]}`;
+  }
+}
